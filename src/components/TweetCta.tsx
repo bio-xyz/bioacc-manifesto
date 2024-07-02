6d0820cb07f8769be160739e5e86ec6a9d4baf79
stadolf <stadolf@gmail.com>
2024-07-02 20:10:28 +0200

    replaces tweet embed with a plain submission response
    Signed-off-by: stadolf <stadolf@gmail.com>

--- src/components/TweetCta.tsx
+++ src/components/TweetCta.tsx
@@ -1,36 +1,21 @@
 'use client'
 
 import { Button } from './ui/button'
-import { Tweet } from 'react-tweet'
 
 export const TWEET_ID = '1796199193461043490'
 export const TWEET_URL =
   'https://twitter.com/bio_xyz/status/1796199193461043490'
 
-export const REPOST_TWEET_INTENT = TWEET_URL //`https://twitter.com/intent/retweet?tweet_id=${TWEET_ID}`
-
 export default function TweetCta() {
   return (
-    <div className="flex flex-col items-center">
-      <div data-theme="dark">
-        <Tweet id={TWEET_ID} />
-      </div>
-      {/* <XEmbed
-        url={TWEET_URL}
-        width={500}
-        twitterTweetEmbedProps={{
-          tweetId: TWEET_ID,
-          options: {
-            theme: 'dark',
-            align: 'center',
-            width: '500',
-          },
-        }}
-      /> */}
-      {/* <Tweet id="https://twitter.com/astrodotbuild/status/1511750228428435457" /> */}
-      <Button asChild className="mt-4 w-full">
-        <a href={REPOST_TWEET_INTENT} target="_blank">
-          Repost this
+    <div className="flex flex-col space-y-12">
+      <p>
+        Thank you for submitting your details. In order to complete signing and
+        be added as a signer, repost the bio/acc manifesto on X.
+      </p>
+      <Button asChild className="mt-4 px-7 w-min">
+        <a href={TWEET_URL} target="_blank">
+          Repost on X
         </a>
       </Button>
     </div>
